45b6d587c7abf0e7c24bd3a1fa769d3b232c4ab6
tayson13 <ab.naser01@gmail.com>
2023-06-25 19:32:55 +0600

    add deposit

--- prac.php
+++ prac.php
@@ -3,10 +3,11 @@
 class menu {
     public static $menu = [
         1 => "Deposit Money",
-        2 => "Withdraw Money",
-        3 => "See Last 5 transaction",
-        4 => "Deactivate  Account",
-        5 => "Leave System"
+        2 => "Deposit Money",
+        3 => "Withdraw Money",
+        4 => "Last 5 transaction",
+        5 => "Deactivate  Account",
+        6 => "Leave System"
     ];
 }
 
@@ -44,17 +45,60 @@ class openAccount extends account {
     }
 }
 
+class deposit extends account{
+    $value = 0;
+
+    function __construct($value){
+        if($value>0){
+            $this->value = $value;
+        }else{
+            echo "something went wrong";
+        }
+        
+    }
+
+    function depositFunction($amount){
+        $this->balance+=$amount;
+    }
+}
+
 $ob1 = new openAccount();
 
     
 while(1){
     echo "\n"."Chose an Option \n \n";
-foreach(menu::$menu as $key => $m){
-    echo $key ." => ". $m ."\n";
-}
-echo "\n";
-
-$x = readline();
-echo $x;
+    foreach(menu::$menu as $key => $m){
+        echo $key ." => ". $m ."\n";
+        
+    }
+    echo "\n";
+    
+    $x = readline();
+    
+    switch ($x) {
+        case "1":
+            echo "\n how much you want to deposit? \n";
+            $deposit = readline();
+            break;
+        case "2":
+            echo "Withdrawing Money";
+            break;
+        case "3":
+            echo "Last 5 transaction";
+            break;
+        case "4":
+            echo "Deactivate the account";
+            break;
+        case "5":
+            echo "Leaving the system";
+            break;
+        case "6":
+            echo "Leaving the system";
+            break;
+      default:
+        echo "Wrong Input";
+    }
+    
+    echo "\n";
 }
 ?>
